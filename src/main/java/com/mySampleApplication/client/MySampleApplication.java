b4feac905216f9e49345be67e4e81c4f4763a89f
Peter Donald <peter@realityforge.org>
2011-07-26 16:18:11 +1000

    Remove service guff

--- src/main/java/com/mySampleApplication/client/MySampleApplication.java
+++ src/main/java/com/mySampleApplication/client/MySampleApplication.java
@@ -57,30 +57,6 @@ public class MySampleApplication implements EntryPoint {
         });
         verticalPanel.add(button);
         RootPanel.get("book_content").add(verticalPanel);
-
-/*
-
-        final Button button = new Button("Click me");
-        final Label label = new Label();
-
-        button.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                if (label.getText().equals("")) {
-                    MySampleApplicationService.App.getInstance().getMessage("Hello, World!", new MyAsyncCallback(label));
-                } else {
-                    label.setText("");
-                }
-            }
-        });
-
-        // Assume that the host HTML has elements defined whose
-        // IDs are "slot1", "slot2".  In a real app, you probably would not want
-        // to hard-code IDs.  Instead, you could, for example, search for all
-        // elements with a particular CSS class and replace them with widgets.
-        //
-        RootPanel.get("slot1").add(button);
-        RootPanel.get("slot2").add(label);
-*/
     }
 
     private void addBookToTable(final FlexTable t, final Book book) {
@@ -109,21 +85,5 @@ public class MySampleApplication implements EntryPoint {
         panel.add(description);
         panel.add(remove);
         t.setWidget(row, 3, panel);
-    }
-
-    private static class MyAsyncCallback implements AsyncCallback<String> {
-        private Label label;
-
-        public MyAsyncCallback(Label label) {
-            this.label = label;
-        }
-
-        public void onSuccess(String result) {
-            label.getElement().setInnerHTML(result);
-        }
-
-        public void onFailure(Throwable throwable) {
-            label.setText("Failed to receive answer from server!");
-        }
     }
 }
--- src/main/java/com/mySampleApplication/client/MySampleApplicationServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.mySampleApplication.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface MySampleApplicationServiceAsync {
-    void getMessage(String msg, AsyncCallback<String> async);
-}
