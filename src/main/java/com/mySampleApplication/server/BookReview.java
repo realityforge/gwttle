6297d0d533fa62b9db808847d928291258d8562b
Peter Donald <peter@realityforge.org>
2011-07-27 14:48:52 +1000

    Add initial book review service

---
+++ src/main/java/com/mySampleApplication/server/BookReview.java
@@ -0,0 +1,50 @@
+package com.mySampleApplication.server;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "review")
+public class BookReview {
+
+    private String bookTitle;
+    private String review;
+    private String reviewer;
+
+    public BookReview() {
+    }
+
+    public BookReview(String title, String reviewer, String review) {
+        this.bookTitle = title;
+        this.reviewer = reviewer;
+        this.review = review;
+    }
+
+    @XmlAttribute(name = "title")
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    @XmlElement(name = "review-text")
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    @XmlAttribute(name = "reviewer")
+    public String getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(String reviewer) {
+        this.reviewer = reviewer;
+    }
+
+}
---
+++ src/main/java/com/mySampleApplication/server/BookReviewService.java
@@ -0,0 +1,33 @@
+package com.mySampleApplication.server;
+
+import com.mySampleApplication.shared.Book;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+
+public class BookReviewService extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        try {
+            final String uri = request.getRequestURI();
+            final String bookTitle = URLDecoder.decode(uri.substring(uri.lastIndexOf("/"), uri.length()));
+
+            final Book book = BookServiceFactory.getBookByTitle(bookTitle);
+            if (null != book) {
+                final BookReview review = new BookReview(book.title, book.author, "Love it!");
+                final PrintWriter out = response.getWriter();
+                response.setContentType("text/xml");
+                final Marshaller m = JAXBContext.newInstance(BookReview.class).createMarshaller();
+                m.marshal(review, out);
+            }
+        } catch (final Exception e) {
+            throw new ServletException(e);
+        }
+    }
+}
