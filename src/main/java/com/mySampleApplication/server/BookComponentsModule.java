542a45987e17b7a93f731c32dbab83af2bee611d
Peter Donald <peter@realityforge.org>
2011-07-28 15:01:51 +1000

    Add some example descriptions of binds

--- src/main/java/com/mySampleApplication/server/BookComponentsModule.java
+++ src/main/java/com/mySampleApplication/server/BookComponentsModule.java
@@ -6,5 +6,8 @@ public class BookComponentsModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(BookService.class).to(BookServiceDAO.class);
+
+        //bind(BookService.class).toProvider(fromJNDI(BookService.class, "ejb/MyEJB");
+        //bind(BookService.class).to(BookServiceDAO.class).toProvider(com.google.inject.name.Names.named("Foo");
     }
 }
